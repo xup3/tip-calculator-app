5b1addcd071467b8c47ee03cea69f0b4bde642d3
xup3 <1422640+xup3@users.noreply.github.com>
2022-03-04 18:46:20 +0100

    chore: adds pinia and i18n packages

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createI18n } from "vue-i18n";
 import App from "./App.vue";
+
+const i18n = createI18n({
+  // something vue-i18n options here ...
+});
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(i18n);
 
 app.mount("#app");
